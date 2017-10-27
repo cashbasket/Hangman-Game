99c5d67a1dffa5465345eb25928d7a6407bfef07
Ryan Machan <rjm21@case.edu>
2017-10-27 17:03:38 -0400

    initial word ideas for game

--- assets/js/chuck.js
+++ assets/js/chuck.js
@@ -1,9 +1,14 @@
-
 $(document).ready(function () {
+	
+	var words = [];
+	words[0] = ['w','a','l','k','e','r'];
+	words[1] = ['r','a','n','g','e','r'];
+	words[2] = ['k','i','c','k','b','o','x','e','r'];
+
 	$('#shrinkBlackness').click( function() {
 		var curPct = parseFloat($('#blackness').width() / $('#chuckHolder').width()) * 100;
 		var newPct = curPct - 20;
 
 		$('#blackness').css('width', newPct + "%");
 	}) 
-})	
+})
